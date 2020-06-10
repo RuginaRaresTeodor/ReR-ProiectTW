9c225197e2101dfa1ff9ea9983dc2279a318a13b
RuginaRaresTeodor <raresrugina@gmail.com>
2020-06-10 20:18:22 +0300

    Add files via upload

--- front-end/fun.js
+++ front-end/fun.js
@@ -1,8 +1,25 @@
 function changepage1() {
     document.getElementsByClassName('first-page')[0].style.display = "none";
     document.getElementsByClassName('second-page')[0].style.display = "block";
+    document.getElementsByClassName('register-page')[0].style.display = "none";
+    document.getElementsByClassName('selector')[0].style.display = "none";
   }
   function changepage2() {
     document.getElementsByClassName('first-page')[0].style.display = "block";
     document.getElementsByClassName('second-page')[0].style.display = "none";
+    document.getElementsByClassName('register-page')[0].style.display = "none";
+    document.getElementsByClassName('selector')[0].style.display = "none";
   }
+  function changepageregister() {
+    document.getElementsByClassName('first-page')[0].style.display = "none";
+    document.getElementsByClassName('second-page')[0].style.display = "none";
+    document.getElementsByClassName('register-page')[0].style.display = "block";
+    document.getElementsByClassName('selector')[0].style.display = "none";
+  }
+  function changepage3() {
+    document.getElementsByClassName('first-page')[0].style.display = "none";
+    document.getElementsByClassName('second-page')[0].style.display = "none";
+    document.getElementsByClassName('register-page')[0].style.display = "none";
+    document.getElementsByClassName('selector')[0].style.display = "block";
+  }
+  
