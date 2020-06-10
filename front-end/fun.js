8036a2f6b986ac7af1a40f958240471e7a42a63c
RuginaRaresTeodor <raresrugina@gmail.com>
2020-06-10 17:35:55 +0300

    Add files via upload

---
+++ front-end/fun.js
@@ -0,0 +1,8 @@
+function changepage1() {
+    document.getElementsByClassName('first-page')[0].style.display = "none";
+    document.getElementsByClassName('second-page')[0].style.display = "block";
+  }
+  function changepage2() {
+    document.getElementsByClassName('first-page')[0].style.display = "block";
+    document.getElementsByClassName('second-page')[0].style.display = "none";
+  }
