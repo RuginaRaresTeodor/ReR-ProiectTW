6ab402948ebee04654c58c55f987daf259cdc659
Mqroman <48622213+Mqroman@users.noreply.github.com>
2020-06-11 22:25:52 +0300

    unic ques id + incadrare in pagina

--- rares/logedon.php
+++ rares/logedon.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <style type="text/css">
 td
 {
@@ -9,11 +10,8 @@ td
 session_start();
 
 if(!isset($_SESSION["id"])){
-    
     $_SESSION["id"] = UniqueMachineID();
     $_SESSION["name"] = "guest";}
-    
-
     function UniqueMachineID($salt = "") {  
         if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {  
               
@@ -42,27 +40,9 @@ if(!isset($_SESSION["id"])){
         return $str;  
     }  
       
-      
+    
 ?>
 
-<?php
-/*
-if (isset($_POST['id_adresa']) && $_POST['id_adresa']!="") {
- $order_id = $_POST['id_adresa'];
- $url = "http://localhost:2000/rares/api.php?id_adresa=".$order_id;
- 
- $client = curl_init($url);
- curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
- $response = curl_exec($client);
- 
- $result = json_decode($response);
- 
- echo "<table >";
- echo "<tr><td>Domeniu:</td><td>$result->domeniu</td></tr>";
- echo "<tr><td><a href=$result->link>$result->titlu</a></td></tr>";
- echo "</table>";
-}*///imi apeleaza rest apiu pentru un id
-?>
 <?php
 // Include config file
 require_once "config.php";
@@ -115,6 +95,37 @@ oci_execute($compiled);
 }}
 ?>
  
+<html>
+
+<head>
+  <title>Recon</title>
+  <meta name="description" content="We recon for you!">
+  <meta name="author" content="Rares Rugina & Marius Roman">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/x-icon" href="profile.png" />
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" type="text/css" href="tabel.css">
+
+  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+  <link rel="stylesheet" type="text/css" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+
+<body>
+  <div class="nav">
+    <img src="logo.svg" alt="logo">
+    <div class="nav-button" id="acc" onclick="changepage1()">feed</div>
+    <div class="nav-button" onclick="changepage3()">settings</div>
+    <div class="nav-button" onclick="changepage2()">Home</div>
+  </div>
+  <div class="register-page2"  >
+   
+      <div class="form-container">
+        </br>  </br>  </br>  </br>
+        
+      
+
+
 <?php
 $conn  =  oci_connect('student', 'student','localhost:1521/xe');
 if (!$conn) {
@@ -125,7 +136,7 @@ if (!$conn) {
 if($_SESSION["id"])
 $id=$_SESSION["id"];
 else
-$id= $_SESSION["id"];
+$id=$_SESSION["id"];
 $stid = oci_parse($conn, "SELECT adresa_id FROM relatii where user_id=$id");
 oci_execute($stid);
 //vezi ce domenii ai si la final parcurgi domeniu cu domeniu
@@ -154,6 +165,7 @@ while ($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
 $domain = array_unique($pars);
 
 foreach ($domain as $i){
+    echo '<div class="topleft">';
     echo "<table class='minimalistBlack'>";
     echo "<thead>
     <tr><th>Domeniu:$i</th></tr></thead>
@@ -170,17 +182,12 @@ while ($row = oci_fetch_array($sti, OCI_ASSOC+OCI_RETURN_NULLS)) {
         echo "<tr><td><a href=$bd_link> $bd_titlu</a></td></tr>";
     }
  } echo "</tbody></table></br>";
+ echo "</div>";
+
 }
  
     ?>
-    
-<html>
-<head>
-<title>User Login</title>
-<link rel="stylesheet" type="text/css" href="tabel.css">
-</head>
-<body>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($k_titlu_err)) ? 'has-error' : ''; ?>">
                 <label>Title</label>
                 <input type="text" name="k_titlu" class="form-control" value="<?php echo $k_titlu; ?>">
@@ -214,5 +221,15 @@ Welcome <?php echo $_SESSION["name"]; ?>. Click here to <a href="logout.php" tit
 <?php
 }else echo "<h1>Please login first .</h1>";
 ?>
+  
+   
+  </div>
+  </div>
+  <div class="selector">
+    
+  </div>
+    
+    
 </body>
+
 </html>
