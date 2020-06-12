606c0d88b6f231731eb0e7793d3da956b33f190c
RuginaRaresTeodor <raresrugina@gmail.com>
2020-06-12 11:51:36 +0300

    Add files via upload

--- rares/popup.js
+++ rares/popup.js
@@ -1,20 +1,26 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var checkPageButton = document.getElementById('checkPage');
-  checkPageButton.addEventListener('click', function() {
+// document.addEventListener('DOMContentLoaded', function() {
+//   var checkPageButton = document.getElementById('checkPage');
+//   checkPageButton.addEventListener('click', function() {
 
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
+//     chrome.tabs.getSelected(null, function(tab) {
+//       d = document;
 
-      var f = d.createElement('form');
-      f.action = 'http://gtmetrix.com/analyze.html?bm';
-      f.method = 'post';
-      var i = d.createElement('input');
-      i.type = 'hidden';
-      i.name = 'url';
-      i.value = tab.url;
-      f.appendChild(i);
-      d.body.appendChild(f);
-      f.submit();
-    });
-  }, false);
-}, false);
+//       var f = d.createElement('form');
+//       f.action = 'http://gtmetrix.com/analyze.html?bm';
+//       f.method = 'post';
+//       var i = d.createElement('input');
+//       i.type = 'hidden';
+//       i.name = 'url';
+//       i.value = tab.url;
+//       f.appendChild(i);
+//       d.body.appendChild(f);
+//       f.submit();
+//     });
+//   }, false);
+// }, false);
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    var newURL = "http://www.youtube.com/watch?v=oHg5SJYRHA0";
+    chrome.tabs.create({ url: newURL });
+});
+window.open("http://localhost/rares/feed.php",'_blank');
